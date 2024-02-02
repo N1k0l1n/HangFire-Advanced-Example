b73c1ae89fc335cbc92d3ad315175e6981bf83cf
N1k0L1n <nikolinandoni80@gmail.com>
2024-02-02 15:39:56 +0100

    Add project files.

---
+++ Controllers/HangfireController.cs
@@ -0,0 +1,31 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFireDeep.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangfireController : ControllerBase
+    {
+        #region Fire & Forget
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Wellcome()
+        {
+            var jobId = BackgroundJob.Enqueue(() => SendWellcomeEmail("Welcome to our App"));
+
+            return Ok($"JobId: {jobId}. Welcome email sent to user");
+        }
+
+        // Helper function
+        [NonAction]
+        public void SendWellcomeEmail(string text)
+        {
+            Console.WriteLine(text);
+            // Add your logic here to send a welcome email
+        }
+        #endregion
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Hangfire;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Add Hangfire services.
+builder.Services.AddHangfire(configuration =>
+
+configuration.UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection")));
+
+// Add the processing server as IHostedService
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseHangfireDashboard();
+
+app.UseAuthorization();
+
+
+
+app.MapControllers();
+
+app.Run();
